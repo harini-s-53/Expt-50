4e446d4181fd096ffa5c34e1fa33610db5817d90
Harini S <harini.subramaniam@yale.edu>
2025-04-25 11:38:37 -0400

    Add files via upload

---
+++ 5_trim_galore.sh
@@ -0,0 +1,11 @@
+trim_galore --paired --cores 4 --quality 35 --output_dir /home/hs2256/palmer_scratch/Expt-50/human_mapping/trim_gallore_results/ /home/hs2256/palmer_scratch/Expt-50/human_mapping/all_fastq_files/A1_R1.fastq.gz /home/hs2256/palmer_scratch/Expt-50/human_mapping/all_fastq_files/A1_R2.fastq.gz
+
+trim_galore --paired --cores 4 --quality 35 --output_dir /home/hs2256/palmer_scratch/Expt-50/human_mapping/trim_gallore_results/ /home/hs2256/palmer_scratch/Expt-50/human_mapping/all_fastq_files/A2_R1.fastq.gz /home/hs2256/palmer_scratch/Expt-50/human_mapping/all_fastq_files/A2_R2.fastq.gz
+
+trim_galore --paired --cores 4 --quality 35 --output_dir /home/hs2256/palmer_scratch/Expt-50/human_mapping/trim_gallore_results/ /home/hs2256/palmer_scratch/Expt-50/human_mapping/all_fastq_files/A3_R1.fastq.gz /home/hs2256/palmer_scratch/Expt-50/human_mapping/all_fastq_files/A3_R2.fastq.gz
+
+trim_galore --paired --cores 4 --quality 35 --output_dir /home/hs2256/palmer_scratch/Expt-50/human_mapping/trim_gallore_results/ /home/hs2256/palmer_scratch/Expt-50/human_mapping/all_fastq_files/B1_R1.fastq.gz /home/hs2256/palmer_scratch/Expt-50/human_mapping/all_fastq_files/B1_R2.fastq.gz
+
+trim_galore --paired --cores 4 --quality 35 --output_dir /home/hs2256/palmer_scratch/Expt-50/human_mapping/trim_gallore_results/ /home/hs2256/palmer_scratch/Expt-50/human_mapping/all_fastq_files/B2_R1.fastq.gz /home/hs2256/palmer_scratch/Expt-50/human_mapping/all_fastq_files/B2_R2.fastq.gz
+
+trim_galore --paired --cores 4 --quality 35 --output_dir /home/hs2256/palmer_scratch/Expt-50/human_mapping/trim_gallore_results/ /home/hs2256/palmer_scratch/Expt-50/human_mapping/all_fastq_files/B3_R1.fastq.gz /home/hs2256/palmer_scratch/Expt-50/human_mapping/all_fastq_files/B3_R2.fastq.gz
