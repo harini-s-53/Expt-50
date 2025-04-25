1d7720494b6171d437ad0bf3b9f71779fdd7bf65
Harini S <harini.subramaniam@yale.edu>
2025-04-25 12:08:01 -0400

    Add files via upload

---
+++ mouse_mapping/7_RSEM_calcexp.sh
@@ -0,0 +1,33 @@
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-1-A2/50-1-A2_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-1-A2
+
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-1-A3/50-1-A3_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-1-A3
+
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-1-B1/50-1-B1_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-1-B1
+
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-1-B2/50-1-B2_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-1-B2
+
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-1-B3/50-1-B3_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-1-B3
+
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-2-A1/50-2-A1_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-2-A1
+
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-2-A2/50-2-A2_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-2-A2
+
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-2-A3/50-2-A3_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-2-A3
+
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-2-B1/50-2-B1_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-2-B1
+
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-2-B2/50-2-B2_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-2-B2
+
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-2-B3/50-2-B3_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-2-B3
+
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-3-A1_Resub/50-3-A1_Resub_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-3-A1_Resub
+
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-3-A2_Resub/50-3-A2_Resub_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-3-A2_Resub
+
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-3-A3_Resub/50-3-A3_Resub_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-3-A3_Resub
+
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-3-B1_Resub/50-3-B1_Resub_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-3-B1_Resub
+
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-3-B2_Resub/50-3-B2_Resub_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-3-B2_Resub
+
+rsem-calculate-expression -p 10 --no-bam-output --paired-end --bam /home/hs2256/palmer_scratch/Expt-50/aligned_STAR/50-3-B3_Resub/50-3-B3_Resub_Aligned.toTranscriptome.out.bam /home/hs2256/palmer_scratch/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref /home/hs2256/palmer_scratch/Expt-50/counts_RSEM/50-3-B3_Resub
---
+++ mouse_mapping/7_RSEM_prepareref.sh
@@ -0,0 +1,4 @@
+rsem-prepare-reference /gpfs/gibbs/pi/datasets/genomes/Mus_musculus/Ensembl/GRCm38/Sequence/WholeGenomeFasta/genome.fa /vast/palmer/scratch/rothlin/hs2256/genomes/RSEM_STAR_index_2.7.7a/RSEM_ref --gtf /gpfs/gibbs/pi/datasets/genomes/Mus_musculus/Ensembl/GRCm38/Annotation/Genes/genes.gtf --star -p 8
+
+
+
